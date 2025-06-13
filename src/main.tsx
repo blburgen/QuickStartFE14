48c1f64879cd01f4fe7790983e01a36c7b7ed423
Brady Burgener <bradylbsee@gmail.com>
2025-06-13 08:58:13 -0600

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Navigation from './Navigation.tsx'
-import "bootstrap"
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navigation />
     <App />
   </StrictMode>,
 )
