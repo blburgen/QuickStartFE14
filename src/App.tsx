8d7e9908aa02759750e9cfb55c07b79ce447022a
Brady Burgener <bradylbsee@gmail.com>
2025-06-13 12:01:37 -0600

    adds under construction

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ export default function App() {
   return (
     <>
       <Navigation />
+      <h1 className="text-center">Quick Start Front End Weeks 14-18</h1>
+      <h1 className="text-center">Under Construction</h1>
     </>
   )
 }
